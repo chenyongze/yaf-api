fc9fa64bd9cc459a713912fd99b4211dd1cc6ee1
yongze <sapphire.php@gmail.com>
2017-10-30 17:33:44 +0800

    add composer support

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 define("APPLICATION_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
 header('content-Type:text/html;charset=utf-8;');
 
+require_once dirname(__FILE__).'/../vendor/autoload.php';
+
 $app  = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
 $app->bootstrap()->run();
 
